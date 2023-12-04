1c5374529b0eb0fbc352379406bbfe0539abcf12
Donald Yubeaton <nasusneverna@gmail.com>
2023-12-04 16:24:52 -0500

    Matrix generation working

---
+++ src/Wire.java
@@ -0,0 +1,72 @@
+public class Wire {
+    private int wireColor;
+
+    public static final int NO_WIRE = 0;
+    public static final int RED_WIRE = 1;
+    public static final int BLUE_WIRE = 2;
+    public static final int YELLOW_WIRE = 3;
+    public static final int GREEN_WIRE = 4;
+
+    // Default constructor
+    public Wire() {
+        this.wireColor = NO_WIRE;
+    }
+
+    // Getter method for wireColor
+    public int getWireColor() {
+        return wireColor;
+    }
+
+    // Setter method for wireColor
+    public void setWireColor(int wireColor) {
+        this.wireColor = wireColor;
+    }
+    // toString method to return a colored square
+    @Override
+    public String toString() {
+        String reset = "\u001B[0m";
+        String red = "\u001B[31m";
+        String blue = "\u001B[34m";  // ANSI escape code for blue text
+        String yellow = "\u001B[33m";
+        String green = "\u001B[32m";
+        String white = "\u001B[37m";
+
+        switch (wireColor) {
+            case RED_WIRE:
+                return red + "■ " + reset;  // Red solid square
+            case BLUE_WIRE:
+                return blue + "■ " + reset;  // Blue solid square
+            case YELLOW_WIRE:
+                return yellow + "■ " + reset;  // Yellow solid square
+            case GREEN_WIRE:
+                return green + "■ " + reset;  // Green solid square
+            default:
+                return white + "■ " + reset;  // White square for no wire (default)
+        }
+    }
+
+    public static void main(String[] args) {
+        Wire redWire = new Wire();
+        redWire.setWireColor(Wire.RED_WIRE);
+
+        Wire blueWire = new Wire();
+        blueWire.setWireColor(Wire.BLUE_WIRE);
+
+        Wire yellowWire = new Wire();
+        yellowWire.setWireColor(Wire.YELLOW_WIRE);
+
+        Wire greenWire = new Wire();
+        greenWire.setWireColor(Wire.GREEN_WIRE);
+
+        Wire noWire = new Wire(); // Default is NO_WIRE
+
+        // Print all defined colors
+        System.out.println("Red Wire: " + redWire);
+        System.out.println("Blue Wire: " + blueWire);
+        System.out.println("Yellow Wire: " + yellowWire);
+        System.out.println("Green Wire: " + greenWire);
+        System.out.println("No Wire: " + noWire);
+    }
+
+    
+}
---
+++ src/WireDiagram.java
@@ -0,0 +1,147 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class WireDiagram {
+
+    static final int WIRE_DIAGRAM_SIZE = 20;
+
+    static final int NUM_WIRES = 4;
+
+    private Wire[][] wireDiagram;
+
+    // Represents if the algorithm is laying wires on rows or columns
+    private boolean fillingRow;
+
+    private ArrayList<Integer> colorsChosen;  
+    private ArrayList<Integer> rowsChosen;    
+    private ArrayList<Integer> colsChosen;    
+
+    private int isDangerous;
+
+    private int wireToCut;
+
+    public WireDiagram() {
+        wireDiagram = new Wire[WIRE_DIAGRAM_SIZE][WIRE_DIAGRAM_SIZE];
+        colorsChosen = new ArrayList<>(NUM_WIRES);
+        rowsChosen = new ArrayList<>(NUM_WIRES / 2);
+        colsChosen = new ArrayList<>(NUM_WIRES / 2);
+    }
+
+    private void initializeDiagram() {
+        for (int i = 0; i < WIRE_DIAGRAM_SIZE; i++) {
+            for (int j = 0; j < WIRE_DIAGRAM_SIZE; j++) {
+                wireDiagram[i][j] = new Wire(); 
+            }
+        }
+    }
+
+    public void generateDiagram() {
+        initializeDiagram();
+
+        if (new Random().nextDouble() >= 0.5) {
+            fillingRow = true;
+        } else {
+            fillingRow = false;
+        }
+
+        Random random = new Random();
+
+        for(int i = 0; i < 4; i ++){
+            int chosenRowOrCol = random.nextInt(WIRE_DIAGRAM_SIZE);
+
+        if(fillingRow){
+            while (rowsChosen.contains(chosenRowOrCol)) {
+                chosenRowOrCol = random.nextInt(WIRE_DIAGRAM_SIZE);
+            }
+        }
+        else{
+            while (colsChosen.contains(chosenRowOrCol)) {
+                chosenRowOrCol = random.nextInt(WIRE_DIAGRAM_SIZE);
+            }
+        }
+
+        int color = random.nextInt(NUM_WIRES) +1;
+
+        while(colorsChosen.contains(color)){
+            color = random.nextInt(NUM_WIRES) +1;
+        }
+
+        if(fillingRow){
+            for(int col = 0; col < WIRE_DIAGRAM_SIZE; col ++){
+                wireDiagram[chosenRowOrCol][col].setWireColor(color);
+            }
+        }
+        else{
+            for(int row = 0; row < WIRE_DIAGRAM_SIZE; row ++){
+                wireDiagram[row][chosenRowOrCol].setWireColor(color);
+            }
+        }
+
+        if(fillingRow){
+            rowsChosen.add(chosenRowOrCol);
+        }
+        else{
+            colsChosen.add(chosenRowOrCol);
+        }
+
+        colorsChosen.add(color);
+
+        fillingRow = !fillingRow;
+        }
+
+        setLabels();
+
+    }
+
+    private void setLabels(){
+        if(colorsChosen.indexOf(Wire.RED_WIRE) < colorsChosen.indexOf(Wire.YELLOW_WIRE)){
+            isDangerous = 1;
+        }
+        else{
+            isDangerous = 0;
+        }
+
+        if(isDangerous == 0){
+            wireToCut = Wire.NO_WIRE;
+        }
+        else{
+            wireToCut = colorsChosen.get(2);
+        }
+
+        System.out.println("isDangerous: " + isDangerous);
+        System.out.println("Wire to Cut: " + wireToCut);
+
+    }
+
+    // Getter method for a particular cell of the wireDiagram matrix
+    public Wire getWireAt(int row, int col) {
+        return wireDiagram[row][col];
+    }
+
+    // Getter method for the isDangerous property
+    public int getIsDangerous() {
+        return isDangerous;
+    }
+
+    // Getter method for the wireToCut property
+    public int getWireToCut() {
+        return wireToCut;
+    }
+
+    public void printDiagram() {
+        for (int i = 0; i < WIRE_DIAGRAM_SIZE; i++) {
+            for (int j = 0; j < WIRE_DIAGRAM_SIZE; j++) {
+                System.out.print(wireDiagram[i][j].toString() + " ");
+            }
+            System.out.println(); // Move to the next line for the next row
+        }
+    }
+
+    public static void main(String[] args) {
+        WireDiagram wireDiagram = new WireDiagram();
+        wireDiagram.generateDiagram();
+        wireDiagram.printDiagram();
+    }
+
+
+}
